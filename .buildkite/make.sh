34e4c738f94c21486d7d7ee2108fd2af29809cef
Karan Thukral <karanthukral@gmail.com>
2017-08-10 14:00:21 -0400

    Add .buildkite dir

---
+++ .buildkite/make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -ex
+
+: ${PIPA_IMAGE_FULL_NAME?Missing Pipa environment}
+
+export REGISTRY=$(echo $PIPA_IMAGE_FULL_NAME | cut -d':' -f1)
+export TAG=$(echo $PIPA_IMAGE_FULL_NAME | cut -d':' -f2)
+export docker=docker
+export ARCH='amd64'
+
+if [[ "$#" -ne 1 ]]; then
+  echo "$0 [image]"
+  exit 1
+fi
+
+docker run -v $PWD:/go/src/k8s.io/ingress -w /tmp/src golang:1.8 make -C /go/src/k8s.io/ingress/controllers/nginx build
+
+make -C controllers/nginx container
