16470cceb11040be8261f11d12a29e0171e60bed
Francisco Mejia <francisco.mejia@shopify.com>
2018-04-04 13:46:40 -0400

    Fix CI e2e problem

--- test/e2e/wait-for-nginx.sh
+++ test/e2e/wait-for-nginx.sh
@@ -30,7 +30,7 @@ kubectl set image \
     deployments \
     --namespace ingress-nginx \
 	--selector app=ingress-nginx \
-    nginx-ingress-controller=quay.io/kubernetes-ingress-controller/nginx-ingress-controller:test
+    nginx-ingress-controller=Shopify/nginx-ingress:test
 
 sleep 5
 
