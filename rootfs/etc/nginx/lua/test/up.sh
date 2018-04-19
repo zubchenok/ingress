6aba6be2aa7366db9b5fd39b85f08d5a552994b8
Zenara Daley <zenara.daley@shopify.com>
2018-04-19 11:31:44 -0400

    fix up.sh round 2

--- rootfs/etc/nginx/lua/test/up.sh
+++ rootfs/etc/nginx/lua/test/up.sh
@@ -6,5 +6,5 @@ if luarocks list --porcelain busted $BUSTEDVERSION | grep -q "installed"; then
   echo busted already installed, skipping ;
 else
   echo busted not found, installing via luarocks...;
-  luarocks install busted $BUSTEDVERSION;
+  sudo luarocks install busted $BUSTEDVERSION;
 fi
